f6eb134bd6ebc6142bf3d302002a57d4ec47fef9
agta <angeloabrita@gmail.com>
2021-12-26 23:53:26 -0300

    comment dotenv

--- serve.py
+++ serve.py
@@ -1,8 +1,9 @@
 import os
 
+#using the localhost address
+# from dotenv import load_dotenv, find_dotenv
+# load_dotenv(find_dotenv())
 
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 import flask
